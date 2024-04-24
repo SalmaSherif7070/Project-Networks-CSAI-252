a52c2da48ba37621d68f9415a1a020714b4dba82
Salma Ali <202200622@zewailcity.edu.eg>
2024-04-24 22:33:31 +0200

    add UDP SS

---
+++ Receiver.py
@@ -0,0 +1,24 @@
+import socket as s
+
+
+def acknowledgement(packet):
+    packet_id = packet[0:16]
+    file_id = packet[16:32]
+    # message = packet[32:-32]
+    # Trailer = packet[-32:]
+    return packet_id + file_id
+
+
+
+s2 = s.socket(s.AF_INET, s.SOCK_DGRAM)
+port = 12345
+address = ('127.0.0.1', port)
+
+
+data, server = s2.recvfrom(4064)
+
+acknowledgement = acknowledgement(data.decode()).encode()
+sent = s2.sendto(acknowledgement, address)
+
+print(f'received {data.decode()}')
+s2.close()
--- Sender.py
+++ Sender.py
@@ -1,8 +1,7 @@
-import socket
 import os
 from PIL import Image
 import math
-
+import socket as s
 
 
 # Step 1
@@ -22,9 +21,9 @@ def divide_bits_into_chunks(file_id, bits, chunk_size, num_of_chunks):
     packets = []
     for i in range (math.ceil(num_of_chunks)):
         if i != math.ceil(num_of_chunks)-1:
-            packets.append({"Packet ID": i, "File ID": file_id, "Application Data" : bits[0:chunk_size], "Trailer": 0x0000})
+            packets.append(str(bin(i)[2:].zfill(16)) +  str(bin(file_id)[2:].zfill(16)) + bits[0:chunk_size] + str(bin(0x0000)[2:].zfill(32)))
         else:
-            packets.append({"Packet ID": i, "File ID": file_id,"Application Data" : bits[0:chunk_size], "Trailer": 0xFFFF})
+            packets.append(str(bin(i)[2:].zfill(16)) +  str(bin(file_id)[2:].zfill(16)) + bits[0:chunk_size] + str(bin(0xFFFF)[2:].zfill(32)))
         bits = bits[chunk_size:]
     return packets
 
@@ -34,5 +33,16 @@ bits = image_to_bits(image_path)
 chunk_size = 1000
 file_id = 0
 num_of_chunks = len(bits) / chunk_size
-pakets = divide_bits_into_chunks(file_id, bits, chunk_size, num_of_chunks)
-print(pakets)
+packets = divide_bits_into_chunks(file_id, bits, chunk_size, num_of_chunks)
+# print(packets[-1])
+
+
+
+s1 = s.socket(s.AF_INET, s.SOCK_DGRAM)
+port = 12345
+
+s1.bind(('127.0.0.1', port))
+
+for i in range (len(packets)):
+    data, addr = s1.recvfrom(4064)
+    s1.sendto(packets[i].encode(), addr)
